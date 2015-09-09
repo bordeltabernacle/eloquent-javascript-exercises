c93af4ddce7bc9e433bd1d35e6db4462a64460f4
Rob Phoenix <robertphoenix@gmail.com>
2015-09-09 09:51:04 +0100

    refactored recursion.js after looking at alternative solutions

--- ch03_functions/recursion.js
+++ ch03_functions/recursion.js
@@ -1,22 +1,15 @@
 function isEven(number) {
-    if(number > 0) {
-        if(number - 2 === 0) {
-            return true;
-        } else if(number - 2 === 1) {
-            return false;
-        } else {
-            return isEven(number - 2);
-        }
-    } else {
-        if(number + 2 === 0) {
-            return true;
-        } else if(number + 2 === 1) {
-            return false;
-        } else {
-            return isEven(number + 2);
-        }
+    if(number < 0) {
+        number *= number;
     }
-
+    if(number === 0) {
+        return true;
+    } else if(number === 1) {
+        return false;
+    } else {
+        return isEven(number - 2);
+    }
 }
 
-console.log(isEven(-10));
+
+console.log(isEven(-11));
