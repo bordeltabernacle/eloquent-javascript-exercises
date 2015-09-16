74c9c2087aa2a10e975c2c05ff6c25fba8df051e
Rob Phoenix <robertphoenix@gmail.com>
2015-09-16 10:35:41 +0100

    updated fizzBuzz.js to use a logical short circuit and better if statements, as per the answer http://eloquentjavascript.net/code/#2.2

--- ch02_programStructure/fizzBuzz.js
+++ ch02_programStructure/fizzBuzz.js
@@ -1,11 +1,8 @@
 for(i =1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
+    var output = "";
+    if (i % 3 === 0)
+        output += 'Fizz';
+    if (i % 5 === 0)
+        output += 'Buzz';
+    console.log(output || i);
 }
