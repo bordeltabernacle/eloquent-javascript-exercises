f971746c2c8e4c3c7f2f24a4ab16d14d53d93bbd
Rob Phoenix <rob.phoenix@bt.com>
2015-10-05 11:47:44 +0100

    updated chessBoard.js after properly understanding the code sandbox solution

--- ch02_programStructure/chessBoard.js
+++ ch02_programStructure/chessBoard.js
@@ -1,21 +1,37 @@
 var size = 8;
-var chessString = ' ';
+var chessBoard = "";
 
-for (var j = 1; j <= size; j++) {
-    for (var i = 0; i <= (size - 2); i++) {
-        if (i % 2 === 0) {
-            chessString += '#';
+for (var j = 0; j < size; j++) {
+    // j equates to the x axis
+    for (var i = 0; i < size; i++) {
+        // i equates to the y axis
+        if ((i + j) % 2 === 0) {
+            // (i + j) gives us a point on the intersection between the x axis & the y axis
+            // if this point is even make it a 'black' square, otherwise make it a 'white' square
+            chessBoard += " ";
         } else {
-            chessString += ' ';
+            chessBoard += "#";
         }
     }
-    if (j < size) {
-        if (j % 2 === 0) {
-            chessString += '\n ';
-        } else {
-            chessString += '\n';
-        }
-    }
+    chessBoard += "\n";
+    // at the end of each x axis add a newline in order to move to the next point on the y axis
 }
 
-console.log(chessString);
+console.log(chessBoard);
+
+// to illustrate the mathematical decision for when a square should be black (even) or white (odd).
+
+var numberBoard = "";
+
+for (var j = 0; j < size; j++) {
+    for (var i = 0; i < size; i++) {
+        if ((i + j) % 2 === 0) {
+            numberBoard += "even\t";
+        } else {
+            numberBoard += "odd\t";
+        }
+    }
+    numberBoard += "\n";
+}
+
+console.log(numberBoard);
